57959ce334a2d5a7966d94048942939f78e6537f
sypniewski20 <sypnieski20@gmail.com>
2026-04-14 14:53:53 +0200

    Makefile with snapshot

--- containers/make_sif.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-GATK_VERSION=$(awk -F'\t' '$1=="GATK"{print $2}' build_manifest.tsv)
-IMAGE="pon-benchmark-gatk${GATK_VERSION}"
-
-singularity build ${IMAGE}.sif docker-daemon://${IMAGE}
